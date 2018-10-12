f823f6c866f83c71e74236e2a58ff1d246450d84
Alena Fomina <32820331+fominaalyona@users.noreply.github.com>
2018-10-12 17:51:47 +0300

    Add files via upload

--- AT_MailTest/src/test/java/by/epam/at/mailtest/pages/MainPage.java
+++ AT_MailTest/src/test/java/by/epam/at/mailtest/pages/MainPage.java
@@ -1,8 +1,6 @@
 package by.epam.at.mailtest.pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,7 +14,7 @@ public class MainPage extends AbstractPage{
     private final String DOMAIN = "mailbox:domain";
     private final String PASSWORD = "//div[@class=\"mailbox__input__container\"]/input[@id=\"mailbox:password\"]";
     private final String BUTTON = "//label[@id=\"mailbox:submit\"]/input[@class=\"o-control\"]";
-    private final String LOGEDNAME = "PH_user-email";
+    private final String LOGEDNAME = "PH_user-email1";
 
     @FindBy(xpath = LOGIN)
     private WebElement textBoxLogin;
@@ -51,7 +49,11 @@ public class MainPage extends AbstractPage{
     }
 
     public String getLoggedInUserName() {
-        wait.until(ExpectedConditions.visibilityOf(logedName));
+        try{
+        wait.until(ExpectedConditions.visibilityOf(logedName));}
+        catch (NoSuchElementException | TimeoutException e) {
+            return "Can't find such element on page!";
+        }
         return logedName.getText();
     }
 }
