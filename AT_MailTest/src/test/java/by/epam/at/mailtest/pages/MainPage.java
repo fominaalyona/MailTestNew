a8dfcccde66d6dccdb5c5b85f8b09813e786f32b
Alena Fomina <32820331+fominaalyona@users.noreply.github.com>
2018-10-12 16:57:36 +0300

    Add files via upload

--- AT_MailTest/src/test/java/by/epam/at/mailtest/pages/MainPage.java
+++ AT_MailTest/src/test/java/by/epam/at/mailtest/pages/MainPage.java
@@ -49,6 +49,11 @@ public class MainPage extends AbstractPage{
     }
 
     public String getLoggedInUserName() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return logedName.getText();
     }
 }
