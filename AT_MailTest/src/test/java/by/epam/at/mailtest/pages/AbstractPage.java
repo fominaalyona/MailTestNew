3058cbe44de3142a6eeda5ca82df84ade9ea2ff1
Alena Fomina <32820331+fominaalyona@users.noreply.github.com>
2018-10-12 17:43:51 +0300

    Add files via upload

--- AT_MailTest/src/test/java/by/epam/at/mailtest/pages/AbstractPage.java
+++ AT_MailTest/src/test/java/by/epam/at/mailtest/pages/AbstractPage.java
@@ -1,16 +1,18 @@
 package by.epam.at.mailtest.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class AbstractPage {
 
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     public abstract void openPage();
 
     public AbstractPage(WebDriver driver) {
-
         this.driver = driver;
-
+        this.wait = new WebDriverWait(driver, 10);
     }
+
 }
--- AT_MailTest/src/test/java/by/epam/at/mailtest/pages/MainPage.java
+++ AT_MailTest/src/test/java/by/epam/at/mailtest/pages/MainPage.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MainPage extends AbstractPage{
 
@@ -49,11 +51,7 @@ public class MainPage extends AbstractPage{
     }
 
     public String getLoggedInUserName() {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        wait.until(ExpectedConditions.visibilityOf(logedName));
         return logedName.getText();
     }
 }
